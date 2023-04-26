451a7efa3ac37884d40c81a27112a758922799b6
Chanhyun Wang <rubberramen@naver.com>
2023-04-27 00:32:26 +0900

    Add Log Interceptor

---
+++ src/main/java/com/eatclipseV2/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.eatclipseV2.config;
+
+import com.eatclipseV2.Interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/js/**", "/error");
+    }
+}
