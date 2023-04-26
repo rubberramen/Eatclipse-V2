9c42dfcb0563f74834ef14f0532ce4f57b0b3dbd
Chanhyun Wang <rubberramen@naver.com>
2023-04-27 00:15:30 +0900

    Project Setting

---
+++ src/main/java/com/eatclipseV2/EatclipseV2Application.java
@@ -0,0 +1,13 @@
+package com.eatclipseV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatclipseV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatclipseV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/eatclipseV2/EatclipseV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatclipseV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatclipseV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
